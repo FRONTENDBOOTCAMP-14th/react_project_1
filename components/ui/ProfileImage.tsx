8b9cce4258a7c98fb680cd5548cd407b8f0b2a4f
but212 <wjddlf211@naver.com>
2025-10-09 21:47:02 +0900

    docs: ProfileImage에 jsdoc추가

--- components/ui/ProfileImage.tsx
+++ components/ui/ProfileImage.tsx
@@ -2,6 +2,13 @@ import type { ImgProps } from 'next/dist/shared/lib/get-img-props'
 import Image from 'next/image'
 import styles from './ProfileImage.module.css'
 
+/**
+ * 프로필 이미지 컴포넌트
+ *
+ * @param props - 이미지 props
+ * @param [props.size='small'] - 이미지 크기 ('big'|'small')
+ * @returns 프로필 이미지 컴포넌트
+ */
 const ProfileImage = ({ src, alt, size = 'small' }: ImgProps & { size?: 'big' | 'small' }) => {
   return (
     <Image

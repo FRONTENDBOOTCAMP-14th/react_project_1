b348b2d94ead86cd77a28387d950446c54ed2116
but212 <wjddlf211@naver.com>
2025-10-12 21:49:06 +0900

    style: FIllLink와 FillButton이  min-width로 해서 상위 & 하위 요소에 영향을 받도록 함

--- app/ui-component-test/page.tsx
+++ app/ui-component-test/page.tsx
@@ -1,12 +1,13 @@
 'use client'
 
 import { FillButton, FillLink, ParticipateRate } from '@/components/ui'
+import styles from './page.module.css'
 
 export default function Page() {
   return (
-    <div>
+    <div className={styles['ui-component-test']}>
       <h1>UI Component Test Page</h1>
-      <FillButton onClick={() => alert('Button Clicked!')}>Click Me</FillButton>
+      <FillButton onClick={() => alert('Button Clicked!')}>로그인</FillButton>
       <FillLink href="/">Fill Link</FillLink>
       <ParticipateRate name="사용자" value={75} max={100} />
     </div>

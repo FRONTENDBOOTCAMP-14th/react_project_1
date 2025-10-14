e02416843a0168503c1ec99e91856f16310f84b7
Jeong-il Suk <wjddlf211@naver.com>
2025-10-14 16:47:03 +0900

    refactor: studycard -> studyprofile로 변경 및 _components로 Path 변경

--- app/community/[id]/StudyCard.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-export default function StudyCard({ id }: { id: string }) {
-  return (
-    <div>
-      <h1>StudyCard {id}</h1>
-    </div>
-  )
-}
---
+++ app/community/[id]/_components/StudyProfile.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+export default function StudyProfile({ id }: { id: string }) {
+  return (
+    <div>
+      <h1>StudyProfile {id}</h1>
+    </div>
+  )
+}

94d9a66e60024b79a49014437b820f312f1c33e8
Jeong-il Suk <wjddlf211@naver.com>
2025-10-14 14:13:36 +0900

    refactor: study -> community로 이름 변경

---
+++ app/community/[id]/StudyCard.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+export default function StudyCard({ id }: { id: string }) {
+  return (
+    <div>
+      <h1>StudyCard {id}</h1>
+    </div>
+  )
+}
--- app/community/[id]/page.tsx
+++ app/community/[id]/page.tsx
@@ -1,11 +1,12 @@
 'use client'
 
+import styles from '@/app/study/[id]/page.module.css'
 import { useParams } from 'next/navigation'
 
 export default function Page() {
   const { id } = useParams()
   return (
-    <div>
+    <div className={styles.container}>
       <h1>Study Page {id}</h1>
     </div>
   )

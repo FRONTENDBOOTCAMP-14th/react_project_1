5963205bdccbfd26a9035fdc394dcfa528f04755
Jeong-il Suk <wjddlf211@naver.com>
2025-10-15 09:07:07 +0900

    feat: 공지 링크 추가

--- app/community/[id]/page.tsx
+++ app/community/[id]/page.tsx
@@ -2,6 +2,7 @@
 
 import { useParams } from 'next/navigation'
 import StudyProfile from './_components/StudyProfile'
+import Link from 'next/link'
 
 export default function Page() {
   const { id } = useParams()
@@ -9,6 +10,9 @@ export default function Page() {
     <>
       <StudyProfile id={id as string} />
       <p>리액트를 연습하는 스터디 입니다.</p>
+      <Link href={`/community/${id}/notification`}>
+        <span>공지</span> 노트북 대여는 불가합니다
+      </Link>
     </>
   )
 }

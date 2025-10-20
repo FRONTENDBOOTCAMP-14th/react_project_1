6645a465d93b0daff4b6c869f60bbb35456853c7
Jeong-il Suk <wjddlf211@naver.com>
2025-10-20 16:45:43 +0900

    feat: label 유무 추가

--- components/ui/TextInput.tsx
+++ components/ui/TextInput.tsx
@@ -7,18 +7,23 @@ import styles from './TextInput.module.css'
 export interface TextInputProps extends InputHTMLAttributes<HTMLInputElement> {
   error?: boolean
   ref?: Ref<HTMLInputElement>
+  label?: string
 }
 
 // TODO: Label 컴포넌트와 함께 사용 고려, error 메시지 표시 기능 추가 고려
-const TextInput = ({ type = 'text', className, error, ref, ...rest }: TextInputProps) => {
+const TextInput = ({ type = 'text', className, error, ref, label, ...rest }: TextInputProps) => {
   return (
-    <input
-      ref={ref}
-      type={type}
-      className={cn(className, styles['text-input'], error && styles['text-input--error'])}
-      aria-invalid={error}
-      {...rest}
-    />
+    <>
+      {label && <label htmlFor={label}>{label}</label>}
+      <input
+        ref={ref}
+        type={type}
+        id={label}
+        className={cn(className, styles['text-input'], error && styles['text-input--error'])}
+        aria-invalid={error}
+        {...rest}
+      />
+    </>
   )
 }
 

1981a0751f780c1f75cd364d73efe7205146cfdb
Jeong-il Suk <wjddlf211@naver.com>
2025-10-20 10:02:52 +0900

    feat: console.log -> toast로 변경

--- app/community/new/_components/ImageUploader.tsx
+++ app/community/new/_components/ImageUploader.tsx
@@ -4,6 +4,8 @@ import { IconButton } from '@/components/ui'
 import { Camera } from 'lucide-react'
 import DefaultImg from '@/app/community/new/_components/assets/default-study01.png'
 import styles from './ImageUploader.module.css'
+import Image from 'next/image'
+import { toast } from 'sonner'
 
 /**
  * 이미지 업로드 컴포넌트
@@ -11,29 +13,21 @@ import styles from './ImageUploader.module.css'
  * - 사용자가 이미지를 선택하는 컴포넌트 입니다.
  * - 썸네일 클릭 시 이미지를 삭제하고, 업로드 버튼으로 이미지를 선택합니다.
  *
- * @param {Object} props - 컴포넌트 props
- * @param {(file: File | null) => void} [props.onImageChange] - 이미지 선택 시 호출되는 콜백 함수
- * @returns {JSX.Element} 선택한 이미지 업로드 컴포넌트
- *
  * @example
- * <ImageUploader onImageChange={(file) => console.log(file)} />
+ * <ImageUploader />
  */
 
-interface ImageUploaderProps {
-  onImageChange?: (file: File | null) => void
-}
-
-export default function ImageUploader({ onImageChange }: ImageUploaderProps) {
-  console.log(onImageChange)
-  // 기능은 추가중
-  const handleThumbnailClick = () => {
+export default function ImageUploader() {
+  const handleThumbnailClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    event.stopPropagation()
     // 이미지 삭제
-    console.log('썸네일 클릭 - 삭제')
+    toast('썸네일 클릭 - 삭제')
   }
 
-  const handleUploadClick = () => {
+  const handleUploadClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.stopPropagation()
     // 파일 선택
-    console.log('업로드 버튼 클릭')
+    toast('업로드 버튼 클릭')
   }
 
   return (
@@ -42,7 +36,13 @@ export default function ImageUploader({ onImageChange }: ImageUploaderProps) {
 
       {/* 이미지 썸네일 영역 */}
       <div className={styles.imageContainer} onClick={handleThumbnailClick}>
-        <img src={DefaultImg.src} alt="스터디 대표 이미지" className={styles.image} />
+        <Image
+          src={DefaultImg.src}
+          alt="스터디 대표 이미지"
+          className={styles.image}
+          width={150}
+          height={150}
+        />
 
         {/* 업로드 버튼 */}
         <div className={styles.button}>
---
+++ app/community/notification/[id]/page.tsx
@@ -0,0 +1,17 @@
+export default function NotificationPage() {
+  return (
+    <div style={{ position: 'relative' }}>
+      <button
+        style={{
+          display: 'block',
+          position: 'absolute',
+          top: '-56px',
+          right: '36px',
+        }}
+      >
+        쓰기
+      </button>
+      <div>내용</div>
+    </div>
+  )
+}

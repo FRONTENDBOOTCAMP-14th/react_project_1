05175950b6156a2f472fe6f14116d0f6a1bdcb00
min830-a <llhmyll@naver.com>
2025-10-21 14:04:18 +0900

    feat: 추천 스터디 컴포넌트 UI 작성 홈 화면에 보여질 추천 스터디 컴포넌트 UI - StudyCard.tsx 마크업부분과 컴포넌트 연결 - StudyCard.module.css 연결 및 꾸미기

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -8,7 +8,8 @@ export default function main() {
     title: '하낫 둘 셋! 뛰어!',
     desc: '한강에서 제니랑 빠르게 뛰는 러닝클럽입니다.',
     imageUrl: '',
-    tags: ['러닝', '러닝메이트', '한강', '페이스5분'],
+    tags: ['러닝', '러닝메이트', '한강', '페이스5분', '달려라'],
+    value: '75',
   }
 
   return (
--- app/main/study-card/StudyCard.tsx
+++ app/main/study-card/StudyCard.tsx
@@ -3,7 +3,7 @@
 import { toast } from 'sonner'
 import styles from './StudyCard.module.css'
 
-import { StrokeButton } from '@/components/ui'
+import { StrokeButton, ParticipateRate } from '@/components/ui'
 
 // 스터디 정보 타입
 interface Study {
@@ -11,6 +11,9 @@ interface Study {
   desc: string
   imageUrl: string
   tags: string[]
+  value: number
+  max: number
+  name: string
 }
 
 // 카드 컴포넌트 props 타입
@@ -27,27 +30,44 @@ export default function StudyCard({ study }: StudyCardProps) {
   //   title: '하낫 둘 셋! 뛰어!'
   //   desc: '한강에서 제니랑 빠르게 뛰는 러닝클럽입니다.'
   //   imageUrl: ''
-  //   tags: ['러닝','러닝메이트','한강','페이스5분']
+  //   tags: ['러닝','러닝메이트','한강','페이스5분','달려라']
   // }
 
   return (
     <article className={styles.card}>
       <h2 className="sr-only">스터디 소개</h2>
+      <div className={styles.content}>
+        <p className={styles.title}>{study.title}</p>
+        {/* 스터디대표 이미지는 배경이미지로 표시 */}
+        <p className={styles.desc}>{study.desc}</p>
+        <ParticipateRate name="지난 참여율" value={study.value} max={100} />
+      </div>
+      <div className={styles.join}>
+        <figure
+          className={styles.image}
+          style={{
+            backgroundImage: `url(${study.imageUrl || '/assets/default-study01.png'})`,
+          }}
+          aria-hidden="true"
+          // 기본 이미지 에셋 위치 지정후 url 수정필요
+        />
+        <StrokeButton
+          onClick={() => toast('가입이 완료되었습니다!')}
+          type="button"
+          style={{
+            color: 'var(--accent-color)',
+            border: '1px solid var(--accent-color)',
+          }}
+        >
+          가입하기
+        </StrokeButton>
+      </div>
 
-      <p className={styles.title}>{study.title}</p>
-      {/* 스터디대표 이미지는 배경이미지로 표시 */}
-      <figure
-        className={styles.image}
-        style={{
-          backgroundImage: `url(${study.imageUrl || '/assets/default-study01.png'})`,
-        }}
-        // 이미지 에셋 위치 지정필요
-      />
-      <p className={styles.desc}>{study.desc}</p>
-      <StrokeButton onClick={() => toast('가입이 완료되었습니다!')}>가입하기</StrokeButton>
-      <ul className={styles.tags}>
+      <ul className={styles.tagList}>
         {study.tags.map((tag, index) => (
-          <li key={index}>{tag}</li>
+          <li key={index} className={styles.tag}>
+            {tag}
+          </li>
         ))}
       </ul>
     </article>

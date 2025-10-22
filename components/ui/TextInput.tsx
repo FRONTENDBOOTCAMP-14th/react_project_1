5d1bfb021c67304e52afa206cd2862c6c24f15bb
Jeong-il Suk <wjddlf211@naver.com>
2025-10-22 16:25:39 +0900

    refactor: error정보는 input 외부에서 표시 및 처리 -> error관련 input props 제거

--- components/ui/TextInput.tsx
+++ components/ui/TextInput.tsx
@@ -5,13 +5,11 @@ import type { InputHTMLAttributes, Ref } from 'react'
 import styles from './TextInput.module.css'
 
 export interface TextInputProps extends InputHTMLAttributes<HTMLInputElement> {
-  error?: boolean
   ref?: Ref<HTMLInputElement>
   label?: string
 }
 
-// TODO: Label 컴포넌트와 함께 사용 고려, error 메시지 표시 기능 추가 고려
-const TextInput = ({ type = 'text', className, error, ref, label, ...rest }: TextInputProps) => {
+const TextInput = ({ type = 'text', className, ref, label, ...rest }: TextInputProps) => {
   return (
     <>
       {label && <label htmlFor={label}>{label}</label>}
@@ -19,8 +17,7 @@ const TextInput = ({ type = 'text', className, error, ref, label, ...rest }: Tex
         ref={ref}
         type={type}
         id={label}
-        className={cn(className, styles['text-input'], error && styles['text-input--error'])}
-        aria-invalid={error}
+        className={cn(className, styles['text-input'])}
         {...rest}
       />
     </>

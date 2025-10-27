9d757d5d61a542ebc4f1b0659314c38053245e40
Jeong-il Suk <wjddlf211@naver.com>
2025-10-27 10:19:53 +0900

    refactor: 타입과 네이밍 반영 및 path 변경

--- app/search/SearchRegion.tsx
+++ app/search/SearchRegion.tsx
@@ -3,40 +3,30 @@
 import { useMemo, useState, useEffect } from 'react'
 import { Dropdown } from '@/components/ui'
 import styles from './SearchRegion.module.css'
-
-/**
- * 검색 엔진 컴포넌트
- *
- */
-
-// json 데이터 정의
-interface Region {
-  name: string
-  subArea: string[]
-}
+import type { Region } from '@/lib/types'
 
 export default function SearchRegion() {
   const [regions, setRegions] = useState<Region[]>([])
   const [selectedRegion, setSelectedRegion] = useState('')
-  const [selectedSubArea, setSelectedSubArea] = useState('')
+  const [selectedSubRegion, setSelectedSubRegion] = useState('')
 
   useEffect(() => {
-    import('@/app/api/region/region.json').then(regionJson => {
+    import('@/lib/json/region.json').then(regionJson => {
       setRegions(regionJson.default as Region[])
     })
   }, [])
 
-  //첫번째 드롭다운 : name(json)
+  //첫번째 드롭다운 : region(json)
   const regionOptions = useMemo(
-    () => regions.map(r => ({ value: r.name, label: r.name })),
+    () => regions.map(r => ({ value: r.region, label: r.region })),
     [regions]
   )
 
-  //두번째 드롭다운: subarea(json)
-  const subAreaOptions = useMemo(() => {
-    const found = regions.find(r => r.name === selectedRegion)
+  //두번째 드롭다운: subregion(json)
+  const subRegionOptions = useMemo(() => {
+    const found = regions.find(r => r.region === selectedRegion)
     if (!found) return []
-    return found.subArea.map(sa => ({ value: sa, label: sa }))
+    return found.subRegion.map(sa => ({ value: sa, label: sa }))
   }, [regions, selectedRegion])
 
   return (
@@ -50,14 +40,14 @@ export default function SearchRegion() {
           value={selectedRegion}
           onChange={value => {
             setSelectedRegion(value)
-            setSelectedSubArea('')
+            setSelectedSubRegion('')
           }}
           placeholder="선택하세요"
         />
         <Dropdown
-          options={subAreaOptions}
-          value={selectedSubArea}
-          onChange={value => setSelectedSubArea(value)}
+          options={subRegionOptions}
+          value={selectedSubRegion}
+          onChange={value => setSelectedSubRegion(value)}
           placeholder="선택하세요"
         />
       </div>

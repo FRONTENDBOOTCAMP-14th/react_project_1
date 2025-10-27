d545255dffd77b1ca5e601b047bbf6b56c9f1eeb
Jeong-il Suk <wjddlf211@naver.com>
2025-10-27 10:01:16 +0900

    style: 404 페이지 스타일 개선

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,15 +1,15 @@
 'use client'
 
-import { StrokeButton } from '@/components/ui'
-import Link from 'next/link'
+import styles from './not-found.module.css'
+import { StrokeLink, StrokeButton } from '@/components/ui'
 
 export default function NotFound() {
   return (
-    <div>
+    <div className={styles['not-found']}>
       <h1>페이지를 찾을 수 없습니다.</h1>
       <p>요청하신 페이지가 존재하지 않거나 이동되었을 수 있습니다.</p>
-      <div>
-        <Link href="/">홈으로 돌아가기</Link>
+      <div className={styles['button-container']}>
+        <StrokeLink href="/">홈으로 돌아가기</StrokeLink>
         <StrokeButton onClick={() => globalThis.history.back()}>
           이전 페이지로 돌아가기
         </StrokeButton>
